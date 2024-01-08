874bfe25b404ad1162748603bd6677830f123bf5
vaibhav <vaibhavmasetty06@gmail.com>
2024-01-07 22:39:39 -0500

    first commit

---
+++ QuestionService/src/main/java/com/question/controllers/QuestionController.java
@@ -0,0 +1,41 @@
+package com.question.controllers;
+
+import com.question.entities.Question;
+import com.question.services.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/question")
+public class QuestionController {
+
+    private QuestionService questionService;
+
+    @Autowired
+    public QuestionController(QuestionService questionService) {
+        this.questionService = questionService;
+    }
+
+    @PostMapping
+    public Question create(@RequestBody Question question){
+        return questionService.create(question);
+    }
+
+    @GetMapping
+    public List<Question> getAll(){
+        return questionService.getAll();
+    }
+
+    @GetMapping("{id}")
+    public Question getAll(@PathVariable Long id){
+        return questionService.getById(id);
+    }
+
+    @GetMapping("/quiz/{quizId}")
+    public List<Question> getQuestionsOfQuiz(@PathVariable Long quizId){
+        return questionService.getQuestionsOfQuiz(quizId);
+    }
+
+}
---
+++ QuestionService/src/main/java/com/question/services/QuestionService.java
@@ -0,0 +1,16 @@
+package com.question.services;
+
+import com.question.entities.Question;
+
+import java.util.List;
+
+public interface QuestionService {
+
+    Question create(Question question);
+
+    List<Question> getAll();
+
+    Question getById(Long id);
+
+    List<Question> getQuestionsOfQuiz(Long quizId);
+}
